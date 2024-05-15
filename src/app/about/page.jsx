362a55f344b839d2cb3e96101c09578710e1a673
Matheusbcy <matheusbcy@gmail.com>
2024-05-15 18:53:49 -0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Explore conosco o vasto universo do cinema aqui no IMDb! Somos um grupo de entusiastas dedicados, comprometidos em trazer uma perspectiva única sobre os filmes que moldam nossa cultura e experiência humana. Desde clássicos atemporais até as últimas obras-primas contemporâneas, mergulhamos fundo nas narrativas, na cinematografia e nas performances que nos cativam e nos emocionam.</p>
+      <p>Nossa missão vai além de simplesmente informar sobre lançamentos e críticas; buscamos criar uma comunidade que celebre a diversidade de visões e vozes no mundo do cinema. Aqui, você encontrará análises aprofundadas, entrevistas exclusivas com cineastas e atores, e debates estimulantes sobre temas relevantes da indústria.</p>
+      <p>Seja você um cinéfilo ávido ou alguém que apenas começou a explorar esse universo mágico, convidamos você a se juntar a nós nessa jornada. Vamos desvendar juntos os mistérios, as emoções e as reflexões que o cinema nos oferece, enquanto cultivamos uma comunidade vibrante e apaixonada em torno dessa forma de arte tão poderosa.</p>
+    </div>
+  )
+}
