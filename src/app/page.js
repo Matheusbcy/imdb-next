eb6316b791d1aff2fe3fdec807cb93b33aa71c1c
Matheusbcy <matheusbcy@gmail.com>
2024-05-16 13:30:13 -0300

    add loading effect using next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,22 @@
 const API_KEY = process.env.API_KEY;
-import Results from "../components/Results"
+import Results from "../components/Results";
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}&language=pt-BR&page=1`
+    }?api_key=${API_KEY}&language=pt-BR&page=1`,
+    { next: { revalidade: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
   const results = data.results;
-  return <div>
-    <Results results={results}/>
-  </div>;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
