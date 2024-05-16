0ba350365d9f8bd3258321fac716655c3452b284
Matheusbcy <matheusbcy@gmail.com>
2024-05-16 16:41:08 -0300

    add alts

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -17,6 +17,7 @@ export default async function MoviePage({ params }) {
           height={300}
           className="rounded-lg"
           style={{ maxWidth: "100%", maxHeight: "100%" }}
+          alt="poster"
         ></Image>
         <div className="p-2">
           <h2 className="text-lg mb-3 font-bold">
